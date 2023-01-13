46371e465d627bdd5e787e71fe4fb7e1d38deb23
nawrrar <122539897+nawrrar@users.noreply.github.com>
2023-01-13 08:59:44 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Rabbit)
+    music.playMelody("C5 F D B F D F G ", 120)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Pitchfork)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 200, 1, 255, 0, 9999, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
 })
-basic.showIcon(IconNames.Scissors)
+music.playTone(523, music.beat(BeatFraction.Breve))
