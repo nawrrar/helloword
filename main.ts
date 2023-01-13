11ce2faee5dc40c4721165a3a53ec0acac54ac7d
nawrrar <122539897+nawrrar@users.noreply.github.com>
2023-01-13 08:44:04 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Rabbit)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Pitchfork)
+})
+basic.showIcon(IconNames.Scissors)
